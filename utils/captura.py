88d514e71e68306ba8fb8d4a1bd6da8b20081d78
n-bolanos <nianbolanos@gmail.com>
2025-11-15 15:21:53 -0500

    Creación de módulo para capturar audio

--- utils/captura.py
+++ utils/captura.py
@@ -1,9 +1,20 @@
 import soundfile as sf
 import sounddevice as sd
 
+def leer_audio(path):
+    """
+    Esta función lee un audio que esté en el path indicado.
+    Retorna una tupla (data, frecuencia_muestreo)
+    """
+    return sf.read(path)
 
-# Puede funcionar si tienes las bibliotecas correctas
-data, samplerate = sf.read('audio/Acorde DoMiSol.wav')
-print(data, samplerate)
-sd.play(data, samplerate)
-sd.wait()
+def capturar_audio():
+    """
+    Esta función detecta el audio en vivo.
+    """
+    pass
+
+def reproducir_audio(data, sr):
+    sd.play(data, sr)
+    sd.wait()
+    return
