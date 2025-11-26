f5952c8da53c0d7ecb5667337825583a7d4a7406
n-bolanos <nianbolanos@gmail.com>
2025-11-25 22:28:06 -0500

    Ajuste nuevo audio de prueba

--- utils/captura.py
+++ utils/captura.py
@@ -1,12 +1,20 @@
 import soundfile as sf
 import sounddevice as sd
+import numpy as np
 
 def leer_audio(path):
     """
     Esta función lee un audio que esté en el path indicado.
     Retorna una tupla (data, frecuencia_muestreo)
     """
-    return sf.read(path)
+    data, sr = sf.read(path)
+    
+    if data.ndim == 2:   # stereo → mono
+        data = np.mean(data, axis=1)
+
+    return (data, sr)
+
+
 
 def capturar_audio():
     """
